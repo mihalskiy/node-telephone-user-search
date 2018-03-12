7ba9a12de3f1bb5387864dd9578db42309954290
mihalskiy <mihalskiy.rv@gmail.com>
2018-03-12 17:59:27 +0200

    auth

---
+++ server/models/user.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+      userName: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false
+      },
+      email: {
+          type: DataTypes.STRING,
+          unique: true,
+          allowNull: false
+      },
+      password: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+
+  }, {
+      /*hooks: {
+          beforeCreate: (user) => {
+              const salt = bcrypt.genSaltSync();
+              user.password = bcrypt.hashSync(user.password, salt);
+          }
+      },
+      instanceMethods: {
+          validPassword: function (password) {
+              return bcrypt.compareSync(password, this.password);
+          }
+      }*/
+  });
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  return user;
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,111 @@
 const contactsController = require('../controllers').contacts;
+const User = require('../models').user;
+
+// middleware function to check for logged-in users
+const sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }
+};
 
 module.exports = (app) => {
-  app.get('/', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
-  }));
 
-  app.post('/contacts', contactsController.create);
+    // route for Home-Page
+    app.get('/', sessionChecker, (req, res) => {
+          res.redirect('/login');
+      });
+
+    // route for user signup
+    app.route('/signup')
+        .get(sessionChecker, (req, res) => {
+            res.sendFile(__dirname + '/views/signup.html');
+        })
+        .post((req, res) => {
+            User.create({
+                userName: req.body.userName,
+                email: req.body.email,
+                password: req.body.password
+            })
+                .then(user => {
+                    req.session.user = user.dataValues;
+                    res.redirect('/dashboard');
+                })
+                .catch(error => {
+                    res.redirect('/signup');
+                });
+        });
+
+
+// route for user Login
+    app.route('/login')
+        .get(sessionChecker, (req, res) => {
+            res.sendFile(__dirname +'/views/login.html');
+        })
+        .post((req, res) => {
+            let userName = req.body.userName,
+                password = req.body.password;
+
+            User.findOne({ where: { userName: userName } }).then(function (user) {
+                if (!user) {
+                    res.redirect('/login');
+                } else if (!user.validPassword(password)) {
+                    res.redirect('/login');
+                } else {
+                    req.session.user = user.dataValues;
+                    res.redirect('/dashboard');
+                }
+            });
+        });
+
+
+// route for user's dashboard
+    app.get('/dashboard', (req, res) => {
+        if (req.session.user && req.cookies.user_sid) {
+            res.sendFile(__dirname + '/views/dashboard.html');
+        } else {
+            res.redirect('/login');
+        }
+    });
+
+
+
+// route for user logout
+    app.get('/logout', (req, res) => {
+        if (req.session.user && req.cookies.user_sid) {
+            res.clearCookie('user_sid');
+            res.redirect('/');
+        } else {
+            res.redirect('/login');
+        }
+    });
+
+
+// route for handling 404 requests(unavailable routes)
+    app.use(function (req, res, next) {
+        res.status(404).send("Sorry can't find that!")
+    });
+
+    // start the express server
+    app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
+
+
+
+
+
+// route for handling 404 requests(unavailable routes)
+    app.use(function (req, res, next) {
+        res.status(404).send("Sorry can't find that!")
+    });
+
+    // start the express server
+    app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
+
+
+
+
+    app.post('/contacts', contactsController.create);
   app.get('/contacts', contactsController.list);
-/*
-  app.get('/contacts/:contactId', contactsController.retrieve);
-*/
 };
 
